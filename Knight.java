74fb6017b442521ba1b71b40b82e77de1e9e206d
Adwait Pujari <adwaitpujari08@gmail.com>
2021-07-17 02:40:27 -0500

    Phase 3: Knight Complete

--- Knight.java
+++ Knight.java
@@ -9,5 +9,26 @@ public class Knight extends ChessPiece{
     {
         super(name,newColor,theBoard);
         super.setGraphics("KnightBlack.png","KnightWhite.png");
+
+
+    }
+
+
+    public boolean isValidMove(int currentRow, int currentCol, int futureRow, int futureCol) {
+        boolean validity=false;
+
+        String futureTileColour=super.getBoard().getPieceColor(futureRow,futureCol);
+        String currentTileColour=super.getBoard().getPieceColor(currentRow,currentCol);
+
+        if(futureRow==currentRow+2 && futureCol==currentCol+1 || futureRow==currentRow-2&& futureCol==currentCol+1 ||
+        futureRow==currentRow+2 && futureCol==currentCol-1 || futureRow==currentRow-2&& futureCol==currentCol-1 ||
+        futureCol==currentCol+2 && futureRow==currentRow+1 || futureCol==currentCol-2&& futureRow==currentRow+1 ||
+        futureCol==currentCol+2 && futureRow==currentRow-1 || futureCol==currentCol-2&& futureRow==currentRow-1)
+            validity=true;
+
+        if(currentTileColour==futureTileColour )
+            validity=false;
+
+        return validity;
     }
 }
