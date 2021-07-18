27cb855325ad13ad3742f798dcb1b3d90b2ccc5a
Adwait Pujari <adwaitpujari08@gmail.com>
2021-07-17 22:21:23 -0500

    Bishop and pawn finally total complete

--- Bishop.java
+++ Bishop.java
@@ -11,10 +11,44 @@ public class Bishop extends ChessPiece{
         super.setGraphics("BishopBlack.png","BishopWhite.png");
     }
 
-    
+
     public boolean isValidMove(int currentRow, int currentCol, int futureRow, int futureCol) {
         boolean validity=false;
 
+        int absRow=Math.abs(futureRow-currentRow);
+        int absCol=Math.abs(futureCol-currentCol);
+        String futureTileColour = super.getBoard().getPieceColor(futureRow, futureCol);
+        String currentTileColour = super.getBoard().getPieceColor(currentRow, currentCol);
+
+        if(absRow==absCol)
+            validity=true;
+
+        if(currentRow+currentCol==futureRow+futureCol && absRow==absCol && absRow+absCol>0) {
+            for (int i = 1; i < absRow; i++) {
+                if (futureRow > currentRow ) {
+                    if (getBoard().hasPiece(futureRow - i, futureCol + i))
+                        validity = false;
+                } else if (futureRow < currentRow) {
+
+                    if (getBoard().hasPiece(futureRow + i, futureCol - i))
+                        validity = false;
+                }
+            }
+        }
+        if(currentRow+currentCol!=futureRow+futureCol && absRow==absCol && absRow+absCol>0) {
+            for (int i = 1; i < absRow; i++) {
+                if (futureRow > currentRow ) {
+                    if (getBoard().hasPiece(futureRow - i, futureCol - i))
+                        validity = false;
+                } else if (futureRow < currentRow) {
+
+                    if (getBoard().hasPiece(futureRow + i, futureCol + i))
+                        validity = false;
+                }
+            }
+        }
+        if (currentTileColour.equalsIgnoreCase(futureTileColour))
+            validity = false;
 
 
         return validity;
--- Pawn.java
+++ Pawn.java
@@ -1,66 +1,65 @@
-public class Pawn extends ChessPiece{
+public class Pawn extends ChessPiece {
 
-    public Pawn(String newColor, Board theBoard)
-    {
-        super("Pawn", newColor,theBoard);
-        super.setGraphics("PawnBlack.png","PawnWhite.png");
-       // super.toString();
+    public Pawn(String newColor, Board theBoard) {
+        super("Pawn", newColor, theBoard);
+        super.setGraphics("PawnBlack.png", "PawnWhite.png");
+        // super.toString();
 
     }
-    public Pawn(String name, String newColor, Board theBoard)
-    {
-        super(name,newColor,theBoard);
-        super.setGraphics("PawnBlack.png","PawnWhite.png");
+
+    public Pawn(String name, String newColor, Board theBoard) {
+        super(name, newColor, theBoard);
+        super.setGraphics("PawnBlack.png", "PawnWhite.png");
 
     }
 
 
     public boolean isValidMove(int currentRow, int currentCol, int futureRow, int futureCol) {
-        boolean validity=false;
+        boolean validity = false;
 
-        String futureTileColour=super.getBoard().getPieceColor(futureRow,futureCol);
-        String currentTileColour=super.getBoard().getPieceColor(currentRow,currentCol);
+        String futureTileColour = super.getBoard().getPieceColor(futureRow, futureCol);
+        String currentTileColour = super.getBoard().getPieceColor(currentRow, currentCol);
 
 
-        boolean checkPieceForWhite=super.getBoard().hasPiece(3,currentCol);
-        boolean checkPieceForBlack=super.getBoard().hasPiece(4,currentCol);
+        boolean checkPieceForWhite = super.getBoard().hasPiece(3, currentCol);
+        boolean checkPieceForBlack = super.getBoard().hasPiece(4, currentCol);
 
-        if(currentTileColour=="WHITE" )
-        {
+        if (currentTileColour == "WHITE") {
             //System.out.println(currentRow+" " +currentCol);
-            if(currentRow==1 && !checkPieceForWhite && futureRow==currentRow+2 && futureCol==currentCol)
-                validity=true;
-            if(futureRow==currentRow+1 && futureCol==currentCol)
-            {
-                validity=true;
-            }
-            if(futureTileColour=="BLACK")
-            {
-                validity=false;
-            }
-//            if(super.getBoard().hasPiece(currentRow+1,currentCol+1) || super.getBoard().hasPiece(currentRow+1,currentCol-1))
-//                validity=true;
 
+
+                if (currentRow == 1 && !checkPieceForWhite && futureRow == currentRow + 2 && futureCol == currentCol)
+                    validity = true;
+                if (futureRow == currentRow + 1 && futureCol == currentCol) {
+                    validity = true;
+                }
+                if (futureTileColour == "BLACK") {
+                    validity = false;
+                }
+                if (futureCol == currentCol + 1 && futureRow == currentRow + 1 && getBoard().hasPiece(futureRow, futureCol)
+                        || futureCol == currentCol - 1 && futureRow == currentRow + 1 && getBoard().hasPiece(futureRow, futureCol))
+                    validity = true;
+
+
+
+
+        } else if (currentTileColour == "BLACK") {
+            if (currentRow == 6 && !checkPieceForBlack && futureRow == currentRow - 2 && futureCol == currentCol)
+                validity = true;
+            if (futureRow == currentRow - 1 && futureCol == currentCol) {
+                validity = true;
+            }
+            if (futureTileColour == "WHITE") {
+                validity = false;
+            }
+            if (futureCol == currentCol + 1 && futureRow == currentRow - 1 && getBoard().hasPiece(futureRow, futureCol)
+                    || futureCol == currentCol - 1 && futureRow == currentRow - 1 && getBoard().hasPiece(futureRow, futureCol))
+                validity = true;
 
         }
-        else if(currentTileColour=="BLACK")
-        {
-            if(currentRow==6 && !checkPieceForBlack && futureRow==currentRow-2 && futureCol==currentCol)
-                validity=true;
-            if(futureRow==currentRow-1 && futureCol==currentCol)
-            {
-                validity=true;
-            }
-            if(futureTileColour=="WHITE")
-            {
-                validity=false;
-            }
-//            if(super.getBoard().hasPiece(currentRow-1,currentCol-1) || super.getBoard().hasPiece(currentRow-1,currentCol+1))
-//                validity=true;
-        }
 
-        if(currentTileColour.equalsIgnoreCase(futureTileColour) )
-            validity=false;
+        if (currentTileColour.equalsIgnoreCase(futureTileColour))
+            validity = false;
         return validity;
     }
 }
